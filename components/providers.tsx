3be0c89e404e95a038cc35350f16086c30fe7332
Advait Paliwal <advaitspaliwal@gmail.com>
2024-05-26 16:10:19 -0700

    analytics

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,11 +5,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
+      <TooltipProvider>
+        {children}
+        <Analytics />
+      </TooltipProvider>
       <Toaster expand={false} position="top-center" />
     </NextThemesProvider>
   );

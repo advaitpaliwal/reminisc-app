e6059ca5a87b05c748b839ad0ab8cd0aa88bd929
Advait Paliwal <advaitspaliwal@gmail.com>
2024-06-07 15:36:40 -0700

    switch around

--- components/playground/Playground.tsx
+++ components/playground/Playground.tsx
@@ -7,13 +7,13 @@ import { ChatOutput } from "@/components/playground/ChatOutput";
 export const Playground = () => {
   return (
     <main className="grid h-screen flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+      <ChatOutput />
       <div className="relative hidden flex-col items-start gap-8 md:flex">
         <div className="flex flex-col gap-6 w-full min-w-600px">
           <CreateMemoryForm />
           <MemoriesFieldset />
         </div>
       </div>
-      <ChatOutput />
     </main>
   );
 };
